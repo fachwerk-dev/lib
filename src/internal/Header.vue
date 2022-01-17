7365ce6983b792a67dc10a2d1fd8f807e318eaf6
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-01-17 13:21:22 +0200

    feat: readme and licence take II (#10)

--- src/internal/Header.vue
+++ src/internal/Header.vue
@@ -1,7 +1,23 @@
+# Fachwerk VueJS and Markdown framework for creating interactive learning
+materials https://fachwerk.dev ## Get started ``` npm init fachwerk ``` ##
+Development ### Installation ``` npm install npm run dev ``` ### Testing Run in
+parallel with the development ``` npm run test ``` Run in CI ``` npm run test --
+--run ``` ### Typecheck ``` npm run typecheck ``` ### Building and preview ```
+npm run build npm run preview ``` ### Release ``` npm run release ``` ## Licence
+MIT
 <template>
-  <header class="bg-gray-800 text-gray-200 hover:text-gray-100 p-5 font-medium">
+  <header
+    class="bg-gray-800 text-gray-200 hover:text-gray-100 p-5 font-medium flex justify-between"
+  >
     <ul>
       <li><router-link to="/">Fachwerk</router-link></li>
+    </ul>
+    <ul>
+      <li>
+        <a href="https://github.com/fachwerk-dev/fachwerk" target="_blank">
+          Github
+        </a>
+      </li>
     </ul>
   </header>
 </template>
