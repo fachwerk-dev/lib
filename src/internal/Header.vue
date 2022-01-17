664e8ceb915befc97528937d2c42905eee2b94b9
kristjanjansen <kristjan.jansen@gmail.com>
2022-01-17 13:13:30 +0200

    feat: readme and licence

--- src/internal/Header.vue
+++ src/internal/Header.vue
@@ -1,7 +1,16 @@
 <template>
-  <header class="bg-gray-800 text-gray-200 hover:text-gray-100 p-5 font-medium">
+  <header
+    class="bg-gray-800 text-gray-200 hover:text-gray-100 p-5 font-medium flex justify-between"
+  >
     <ul>
       <li><router-link to="/">Fachwerk</router-link></li>
+    </ul>
+    <ul>
+      <li>
+        <a href="https://github.com/fachwerk-dev/fachwerk" target="_blank">
+          Github
+        </a>
+      </li>
     </ul>
   </header>
 </template>
