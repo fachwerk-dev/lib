1a1e2893097460f4d8a12e28c9c0392bdae62d55
kristjanjansen <kristjan.jansen@gmail.com>
2022-01-29 16:06:26 +0200

    cleanup

--- src/internal/Content.vue
+++ src/internal/Content.vue
@@ -1,6 +1,6 @@
 <template>
   <article
-    class="prose max-w-none p-5 prose-headings:text-gray-800 prose-lead:w-[64ch] prose-p:mt-0 prose-p:w-auto prose-p:before:content-none prose-p:after:content-none prose-a:break-words prose-a:text-sky-700 prose-a:underline prose-a:underline-offset-2 prose-blockquote:border-l-4 prose-blockquote:border-yellow-400 prose-blockquote:pl-6 prose-blockquote:font-normal prose-blockquote:not-italic prose-strong:font-medium prose-code:bg-sky-100 prose-code:px-[0.2em] prose-code:py-[0.1em] prose-code:font-normal prose-code:text-sky-700 prose-code:before:content-none prose-code:after:content-none prose-pre:-mx-5 prose-pre:rounded-none prose-pre:bg-sky-100 prose-pre:p-4 prose-pre:text-sm md:p-12 prose-pre:md:rounded lg:prose-p:w-[64ch] lg:prose-pre:p-6"
+    class="prose max-w-none p-5 prose-headings:text-gray-800 prose-lead:w-[64ch] prose-p:mt-0 prose-p:w-auto prose-p:before:content-none prose-p:after:content-none prose-a:break-words prose-a:text-sky-700 prose-a:underline prose-a:underline-offset-2 prose-blockquote:border-l-4 prose-blockquote:border-yellow-400 prose-blockquote:pl-6 prose-blockquote:font-normal prose-blockquote:not-italic prose-strong:font-medium prose-code:bg-sky-100 prose-code:px-[0.2em] prose-code:py-[0.1em] prose-code:font-normal prose-code:text-sky-700 prose-code:before:content-none prose-code:after:content-none prose-pre:-mx-5 prose-pre:rounded-none prose-pre:bg-sky-100 prose-pre:p-4 prose-pre:text-sm md:p-12 prose-pre:md:-mx-0 prose-pre:md:rounded lg:prose-p:w-[64ch] lg:prose-pre:p-6"
   >
     <slot />
   </article>
