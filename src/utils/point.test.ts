5a7adf488e21287d36850881ca197c40b37914fc
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-10 10:32:24 +0200

    fix: tests

--- src/utils/point.test.ts
+++ src/utils/point.test.ts
@@ -2,7 +2,7 @@ import { test, assert } from "vitest";
 import { rectpoints } from "./point";
 
 test("rectpoints 10,20,30,40", () =>
-  assert.sameDeepOrderedMembers(rectpoints(10, 20, 30, 40), [
+  assert.sameDeepOrderedMembers(rectpoints(30, 40, 10, 20), [
     { x: 10, y: 20 },
     { x: 40, y: 20 },
     { x: 40, y: 60 },
