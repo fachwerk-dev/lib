b76fc61c2a5734fa4da499a6b4f62e9b72e0baac
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-11 14:03:11 +0200

    cleanup

---
+++ docs/sfc.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { compileTemplate } from "@vue/compiler-sfc";
+import template from "./sfc.txt?raw";
+const source = `
+  <f-slider v-model="f.a" />
+  {{ f.a }}
+`;
+const id = "FCompiler.vue";
+const { code } = compileTemplate({ source, id, filename: id });
+const srcdoc = template.replace("CODE", code);
+</script>
+
+<template>
+  <div>
+    <iframe frameborder="0" :srcdoc="srcdoc" />
+  </div>
+</template>
