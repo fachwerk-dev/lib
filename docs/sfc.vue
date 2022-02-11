4809e6ec4b2aa4fac072c0fa3e4241d0fd82318d
kristjanjansen <kristjan.jansen@gmail.com>
2022-02-11 14:15:16 +0200

    cleanup

--- docs/sfc.vue
+++ docs/sfc.vue
@@ -2,8 +2,19 @@
 import { compileTemplate } from "@vue/compiler-sfc";
 import template from "./sfc.txt?raw";
 const source = `
-  <f-slider v-model="f.a" />
-  {{ f.a }}
+<svg width="360" height="50">
+  <rect
+    v-for="h in range(0,360)"
+    :x="h"
+    y="0"
+    width="1"
+    height="50"
+    :fill="hsl(h,100,50)"
+    v-on:mouseover="f.h = h" 
+  />
+</svg>
+
+{{ hsl(f.h,100,50) }}
 `;
 const id = "FCompiler.vue";
 const { code } = compileTemplate({ source, id, filename: id });
