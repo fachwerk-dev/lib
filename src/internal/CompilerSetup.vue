0b8f2127ef67c71ff2cfd91351a945454917c39f
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-03-31 10:08:49 +0300

    cleanup

--- src/internal/CompilerSetup.vue
+++ src/internal/CompilerSetup.vue
@@ -1,26 +1,27 @@
 <script setup lang="ts">
-import { ref, watch, watchEffect } from "vue";
+import { ref, watch } from "vue";
 import { compilerSetup } from "./compiler";
 import template from "./CompilerSetup.txt?raw";
 
 const { content } = defineProps(["content"]);
 const srcdoc = ref("");
 const iframeRef = ref<HTMLIFrameElement | null>(null);
-const height = ref("");
 
 watch(
   [() => content, () => iframeRef.value],
   () => {
     const code = compilerSetup(content);
     srcdoc.value = template.replace("CODE;", code);
-    const frameHeight =
-      iframeRef.value?.contentWindow?.document.body.scrollHeight;
-    console.log(frameHeight);
   },
   { immediate: true }
 );
 </script>
 
 <template>
-  <iframe ref="iframeRef" frameborder="0" :srcdoc="srcdoc" class="h-[75vh]" />
+  <iframe
+    ref="iframeRef"
+    frameborder="0"
+    :srcdoc="srcdoc"
+    class="h-screen md:h-[75vh]"
+  />
 </template>
