e319980c036abe0b037cd943dbdd1dafb7d2d84e
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-05-21 00:57:53 +0300

    fix: hello fixes

--- src/components/FHello.vue
+++ src/components/FHello.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>▦ Hello Fachwerk</h1>
-  <p>It works! <em>Wunderbar!</em> Now, here are some code snippets for you to to copy and try out in your Markdown file or Vue template.</p>
+  <p>It works! <em>Wunderbar!</em> Now, here are some code snippets for you to to copy and try out:</p>
   <details><summary><b>Use global data</b></summary>
   <p>Fachwerk exports a globally available <code>data</code> object, a group of variables that are accessible everywhere. Here is how to set and get the variable <code>data.something</code>:</p>
   <p class="language-">
@@ -9,16 +9,13 @@
   </details>
   <details><summary><b>Use components</b></summary>
   <p>Let's assign the value <code>data.something</code> to something visual, say a <em>radius</em> of the circle:</p>
-  <p class="language-">
-    <pre v-pre><code>&lt;f-svg centered>
+  <div class="language-"><pre v-pre><code>&lt;f-svg centered>
   &lt;circle :r="data.something" />
-&lt;/f-svg></code></pre>
-  </p>
+&lt;/f-svg></code></pre></div>
   </details>
   <details><summary><b>Use functions</b></summary>
   <p>What can be more visual that circle with user-controllabe radius? Perhaps <em>two dozen circles with user-controllable radius forming a single line celebrating a divine beauty of mathematics?</em> Here we go:</p>
-  <p class="language-">
-    <pre v-pre><code>&lt;f-svg centered>
+  <div class="language-"><pre v-pre><code>&lt;f-svg centered>
   &lt;path
     :d="circlepoints(24,data.something / 2)
       .map(point => circlepath(data.something / 2,point))
@@ -28,7 +25,8 @@
     stroke="lightblue"
     stroke-width="2"
   />
-&lt;/f-svg></code></pre></p>
+&lt;/f-svg></code></pre></div>
+  <p>Note that in Vitepress you will need to remove all the newlines from the last code snippet in order to get it working.</p>
   </details>
   <p />
   <p>Read more at Fachwerk <a href="https://fachwerk.dev/" target="_black"> documentation</a>.</p>
