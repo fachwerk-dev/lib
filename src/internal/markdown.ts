c245e179736adb3e0ce95db5b5edc21b660be1cb
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-05-20 07:24:35 +0300

    feat: compiler adjustment

--- src/internal/markdown.ts
+++ src/internal/markdown.ts
@@ -1,24 +1,13 @@
 import MarkdownIt from "markdown-it";
 import MarkdownItExternalLinks from "markdown-it-external-links";
 
-export function MarkdownItFachwerk(md: MarkdownIt) {
-  md.renderer.rules.code_inline = function () {
-    const [tokens, idx, _options, _env, _slf] = arguments;
-    return tokens[idx].content;
-  };
-  md.renderer.rules.code_block = function () {
-    const [tokens, idx, _options, _env, _slf] = arguments;
-    return tokens[idx].content;
-  };
-  return md;
-}
-
 export function compileMarkdown(source: string) {
-  const md = new MarkdownIt({ linkify: true, html: true, breaks: true })
-    .use(MarkdownItFachwerk)
-    .use(MarkdownItExternalLinks, {
+  const md = new MarkdownIt({ linkify: true, html: true, breaks: true }).use(
+    MarkdownItExternalLinks,
+    {
       externalClassName: null,
       externalTarget: "_blank",
-    });
+    }
+  );
   return md.render(source);
 }
