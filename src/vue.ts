aa99d8116525e14c3d0ff85eb3497867bf7e7993
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-08-22 13:03:40 +0300

    cleanup

--- src/vue.ts
+++ src/vue.ts
@@ -1,38 +1,15 @@
-import { defineAsyncComponent, Plugin } from "vue";
-
-// //@ts-ignore
-// export const components = import.meta.glob("./components/*.vue");
-
-// import * as utils from "./utils";
-
-// export const Fachwerk: Plugin = {
-//   install: (app) => {
-//     Object.entries(components).forEach(([path, component]) => {
-//       const name = path.split("/").slice(-1)[0].replace(".vue", "");
-//       app.component(name, defineAsyncComponent(component as any));
-//     });
-//     Object.entries(utils).forEach(
-//       ([name, util]) => (app.config.globalProperties[name] = util)
-//     );
-//   },
-// };
+import { Plugin } from "vue";
 
 export * from "./components";
 export * from "./utils";
 
-const components = import.meta.glob("./components/*.vue", { eager: true });
-console.log(components);
-
 import * as utils from "./utils";
-
-console.log({ ...components });
+import * as components from "./components";
 
 export const Fachwerk: Plugin = {
   install: (app) => {
     Object.entries(components).forEach(([path, component]) => {
-      // console.log(path)
-      //const name = path.split("/").slice(-1)[0].replace(".vue", "");
-      //app.component(name, defineAsyncComponent(component as any));
+      app.component(path, component);
     });
     Object.entries(utils).forEach(
       ([name, util]) => (app.config.globalProperties[name] = util)
